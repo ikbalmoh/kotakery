aba740af8bc5ff798bfb88f553267b5265a9803c
Ikbal <hikmat.iqbal@gmail.com>
2023-05-19 23:18:18 +0700

    Add item to cart, change qty, remove

--- src/app/[username]/components/Products.tsx
+++ src/app/[username]/components/Products.tsx
@@ -3,13 +3,32 @@ import React, { useContext } from 'react';
 import { InboxStackIcon } from '@heroicons/react/24/outline';
 import { currency } from '@/utils/formater';
 import Image from 'next/image';
+import Product from '@/@types/product';
+import { CartItem } from '@/@types/cart';
+import { classNames } from '@/utils/helpers';
+import Button from '@/components/Button';
 
 type Props = {};
 
 export default function Products({}: Props) {
-  const { categories, initializing } = useContext(
+  const { categories, initializing, addToCart, cart, changeQty } = useContext(
     CartContext
   ) as CartContextType;
+
+  const onAddToCart = (product: Product) => {
+    const item: CartItem = {
+      id: product.id!,
+      name: product.name,
+      price: product.price,
+      qty: 1,
+    };
+    addToCart(item);
+  };
+
+  const onCart = (id: string) => cart.find((c) => c.id === id);
+
+  const totalItems: number = cart.reduce((a, b) => a + b.qty, 0);
+  const subtotal: number = cart.reduce((a, b) => a + b.price, 0);
 
   return (
     <div>
@@ -21,38 +40,82 @@ export default function Products({}: Props) {
       {categories.map((category) => (
         <div key={category.id} className="">
           <div className="px-5 py-3 sticky top-16 bg-slate-50 flex items-center">
-            <InboxStackIcon className="w-6 h-6 text-slate-500 mr-3" />
-            <span className="font-semibold text-slate-600 text-lg">
+            <InboxStackIcon className="w-5 h-5 text-slate-500 mr-3" />
+            <span className="font-semibold text-slate-600 text-base">
               {category.name}
             </span>
           </div>
           <div className="grid grid-cols-12 gap-3 px-3 pb-3">
             {category.items?.map((product) => (
               <div
-                className="col-span-6 md:col-span-3 xl:col-span-2 bg-white p-3 rounded-lg shadow-md border border-transparent hover:border-red-500 transition-all"
+                className={classNames(
+                  'col-span-6 md:col-span-3 xl:col-span-2 bg-white p-3 rounded-lg shadow-md border transition-all',
+                  onCart(product.id!) ? 'border-teal-600' : 'border-transparent'
+                )}
                 key={product.id}
               >
                 <div className="font-medium text-base text-slate-800">
                   {product.name}
                 </div>
-                <div className="mt-2">
-                  <span className="text-slate-800 font-semibold text-sm">
+                <div className="mt-1 flex items-end">
+                  <span className="text-red-600 font-semibold text-sm">
                     Rp{currency(product.price)}
                   </span>
-                  <span className="text-xs text-slate-600">
+                  <span className="text-xs text-slate-500">
                     /{product.unit}
                   </span>
                 </div>
-                <div className="flex items-center justify-end mt-3">
-                  <button className="text-xs font-medium rounded-md shadow-md px-3 py-1 border border-red-600 text-red-600 hover:bg-red-50">
-                    PESAN
-                  </button>
+                <div className="flex items-center justify-end mt-5">
+                  {onCart(product.id!) ? (
+                    <div className="rounded-md shadow-md border border-red-600 h-8 flex items-center font-medium bg-white">
+                      <button
+                        className="pl-3 pr-1 text-red-600 h-full text-base"
+                        onClick={() => changeQty(product.id!, '-')}
+                      >
+                        -
+                      </button>
+                      <input
+                        type="text"
+                        value={onCart(product.id!)?.qty}
+                        onChange={(e) =>
+                          changeQty(
+                            product.id!,
+                            e.target.value ? parseInt(e.target.value) : 1
+                          )
+                        }
+                        className="px-3 text-xs outline-none w-10 text-center"
+                      />
+                      <button
+                        className="pr-3 pk-1 text-red-600 h-full text-base"
+                        onClick={() => changeQty(product.id!, '+')}
+                      >
+                        +
+                      </button>
+                    </div>
+                  ) : (
+                    <button
+                      type="button"
+                      onClick={() => onAddToCart(product)}
+                      className="text-xs font-medium rounded-md shadow-md px-3 border border-red-600 text-red-600 hover:bg-red-50 h-8"
+                    >
+                      PESAN
+                    </button>
+                  )}
                 </div>
               </div>
             ))}
           </div>
         </div>
       ))}
+      {totalItems > 0 && (
+        <div className="p-5 bg-white flex items-center sticky bottom-0 intro-y border-t border-red-500">
+          <div className="flex-1 mr-3 flex flex-col">
+            <span className="text-sm text-slate-600">{totalItems} produk</span>
+            <div className="font-bold text-base">Rp{currency(subtotal)}</div>
+          </div>
+          <Button label="Keranjang Belanja" />
+        </div>
+      )}
     </div>
   );
 }
